6207ff20736fb3bb9960bc16228ecffc05c220cd
aripan <aripan017@gmail.com>
2020-09-12 15:45:59 +0200

    Initial Modification and Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBVompMO1CzNGgX5UweppqWHzRA5fCN_30",
+  authDomain: "react--clone-c8de9.firebaseapp.com",
+  databaseURL: "https://react--clone-c8de9.firebaseio.com",
+  projectId: "react--clone-c8de9",
+  storageBucket: "react--clone-c8de9.appspot.com",
+  messagingSenderId: "91292172114",
+  appId: "1:91292172114:web:a49572951fc6582279c48b",
+  measurementId: "G-QBJKY49RBE",
+};
