d86f230e88b957868f618bfae55b01f507c951da
aripan <aripan017@gmail.com>
2020-09-12 23:02:07 +0200

    Initial Setup of Home Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   // BEM convention for naming
@@ -8,7 +9,7 @@ function App() {
     <div className="app">
       <Header />
 
-      {/* Home */}
+      <Home />
     </div>
   );
 }
