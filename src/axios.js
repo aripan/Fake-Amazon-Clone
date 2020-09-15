f44652c8d7e93d23de8b8809820674f00a5118c9
aripan <aripan017@gmail.com>
2020-09-15 18:16:07 +0200

    Connecting with Server

--- src/axios.js
+++ src/axios.js
@@ -2,10 +2,12 @@ import axios from "axios";
 
 const instance = axios.create({
   // THE API (cloud function) URL
-  //   baseURL: 'https://us-central1-challenge-4b2b2.cloudfunctions.net/api'
-  //     // "http://localhost:5001/challenge-4b2b2/us-central1/api",
 
-  baseURL: "http://localhost:5001/react--clone-c8de9/us-central1/api",
+  // Local
+  // baseURL: "http://localhost:5001/react--clone-c8de9/us-central1/api",
+
+  // Cloud FireStore
+  baseURL: "https://us-central1-react--clone-c8de9.cloudfunctions.net/api",
 });
 
 export default instance;
